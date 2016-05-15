b9f72f3dd4725382009dabc080eeb5d237290118
Jamie Morton <jamietmorton@gmail.com>
2016-05-15 16:26:55 -0700

    Adding __init__.py

--- canvas/__init__.py
+++ canvas/__init__.py
@@ -1,2 +1,12 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, canvas development team.
 #
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
 
+from __future__ import absolute_import, division, print_function
+
+
+__version__ = "0.0.1"
