1467f06d85a9256c6fcce2f916db78a4ebab66ac
Jamie Morton <jamietmorton@gmail.com>
2016-05-15 15:46:21 -0700

    BLD: Adding yes to travis

--- canvas/__init__.py
+++ canvas/__init__.py
@@ -1,10 +1,2 @@
-# ----------------------------------------------------------------------------
-# Copyright (c) 2016--, canvas development team.
 #
-# Distributed under the terms of the Modified BSD License.
-#
-# The full license is in the file COPYING.txt, distributed with this software.
-# ----------------------------------------------------------------------------
 
-
-__version__ = "0.0.1-dev"
