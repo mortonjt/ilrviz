8c7a753e5133adfd592fb28d456d17c465245e68
Jamie Morton <jamietmorton@gmail.com>
2016-06-17 16:31:15 -0600

    BLD: Updating __init__.py

--- canvas/kernel/unifrac/__init__.py
+++ canvas/kernel/unifrac/__init__.py
@@ -7,6 +7,6 @@
 # ----------------------------------------------------------------------------
 
 from __future__ import absolute_import, division, print_function
-from .unifrac import aitchison_unifrac, pairwise_aitchison_unifrac
+from canvas.kernel.unifrac import aitchison_unifrac, pairwise_aitchison_unifrac
 
 __all__ = ['aitchison_unifrac', 'pairwise_aitchison_unifrac']
