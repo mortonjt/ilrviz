d796bb75322bd2f0ff00757bd6959f6d6df215f5
Jamie Morton <jamietmorton@gmail.com>
2016-06-25 13:14:54 -0700

    ENH: Adding in balanceplot

---
+++ canvas/viz/tests/test_ilrtree.py
@@ -0,0 +1,248 @@
+from __future__ import division
+from canvas.viz.ilrtree import balance_test, balanceplot
+from skbio import TreeNode
+import numpy as np
+import pandas as pd
+from numpy.random import normal
+import unittest
+
+
+class BalanceTests(unittest.TestCase):
+    def setUp(self):
+        # Basic count data with 2 groupings
+        self.table1 = pd.DataFrame([
+            [10, 10, 10, 20, 20, 20],
+            [11, 12, 11, 21, 21, 21],
+            [10, 11, 10, 10, 11, 10],
+            [10, 11, 10, 10, 10, 9],
+            [10, 11, 10, 10, 10, 10],
+            [10, 11, 10, 10, 10, 11],
+            [10, 13, 10, 10, 10, 12]]).T
+        self.cats1 = pd.Series([0, 0, 0, 1, 1, 1])
+        self.tree1 = TreeNode.read([u"((((((a,b)c, d)e, f)g, h)i, j)k, l);"])
+        # Real valued data with 2 groupings
+        D, L = 40, 80
+        np.random.seed(0)
+        self.table2 = np.vstack((np.concatenate((normal(10, 1, D),
+                                                 normal(200, 1, D))),
+                                 np.concatenate((normal(20, 1, D),
+                                                 normal(100000, 1, D))),
+                                 normal(10, 1, L),
+                                 normal(10, 1, L),
+                                 np.concatenate((normal(20, 1, D),
+                                                 normal(100000, 1, D))),
+                                 normal(10, 1, L),
+                                 normal(10, 1, L),
+                                 normal(10, 1, L),
+                                 normal(10, 1, L)))
+        self.table2 = np.absolute(self.table2)
+        self.table2 = pd.DataFrame(self.table2.astype(np.int).T)
+        self.cats2 = pd.Series([0]*D + [1]*D)
+
+        # Real valued data with 2 groupings and no significant difference
+        self.table3 = pd.DataFrame([
+            [10, 10.5, 10, 10, 10.5, 10.3],
+            [11, 11.5, 11, 11, 11.5, 11.3],
+            [10, 10.5, 10, 10, 10.5, 10.2],
+            [10, 10.5, 10, 10, 10.5, 10.3],
+            [10, 10.5, 10, 10, 10.5, 10.1],
+            [10, 10.5, 10, 10, 10.5, 10.6],
+            [10, 10.5, 10, 10, 10.5, 10.4]]).T
+        self.cats3 = pd.Series([0, 0, 0, 1, 1, 1])
+
+        # Real valued data with 3 groupings
+        D, L = 40, 120
+        np.random.seed(0)
+        self.table4 = np.vstack((np.concatenate((normal(10, 1, D),
+                                                 normal(200, 1, D),
+                                                 normal(400, 1, D))),
+                                 np.concatenate((normal(20, 1, D),
+                                                 normal(100000, 1, D),
+                                                 normal(2000, 1, D))),
+                                 normal(10, 1, L),
+                                 normal(10, 1, L),
+                                 np.concatenate((normal(20, 1, D),
+                                                 normal(100000, 1, D),
+                                                 normal(2000, 1, D))),
+                                 normal(10, 1, L),
+                                 normal(10, 1, L),
+                                 normal(10, 1, L),
+                                 normal(10, 1, L)))
+        self.table4 = np.absolute(self.table4)
+        self.table4 = pd.DataFrame(self.table4.astype(np.int).T)
+        self.cats4 = pd.Series([0]*D + [1]*D + [2]*D)
+
+        # Noncontiguous case
+        self.table5 = pd.DataFrame([
+            [11, 12, 21, 11, 21, 21],
+            [10, 11, 10, 10, 11, 10],
+            [10, 11, 10, 10, 10, 9],
+            [10, 11, 10, 10, 10, 10],
+            [10, 11, 10, 10, 10, 11],
+            [10, 10, 20, 9,  20, 20],
+            [10, 13, 10, 10, 10, 12]]).T
+        self.cats5 = pd.Series([0, 0, 1, 0, 1, 1])
+
+        # Different number of classes case
+        self.table6 = pd.DataFrame([
+            [11, 12, 9, 11, 21, 21],
+            [10, 11, 10, 10, 11, 10],
+            [10, 11, 10, 10, 10, 9],
+            [10, 11, 10, 10, 10, 10],
+            [10, 11, 10, 10, 10, 11],
+            [10, 10, 10, 9,  20, 20],
+            [10, 13, 10, 10, 10, 12]]).T
+        self.cats6 = pd.Series([0, 0, 0, 0, 1, 1])
+
+        # Categories are letters
+        self.table7 = pd.DataFrame([
+            [11, 12, 9, 11, 21, 21],
+            [10, 11, 10, 10, 11, 10],
+            [10, 11, 10, 10, 10, 9],
+            [10, 11, 10, 10, 10, 10],
+            [10, 11, 10, 10, 10, 11],
+            [10, 10, 10, 9,  20, 20],
+            [10, 13, 10, 10, 10, 12]]).T
+        self.cats7 = pd.Series(['a', 'a', 'a', 'a', 'b', 'b'])
+
+        # Swap samples
+        self.table8 = pd.DataFrame([
+            [10, 10, 10, 20, 20, 20],
+            [11, 12, 11, 21, 21, 21],
+            [10, 11, 10, 10, 11, 10],
+            [10, 11, 10, 10, 10, 9],
+            [10, 11, 10, 10, 10, 10],
+            [10, 11, 10, 10, 10, 11],
+            [10, 13, 10, 10, 10, 12]]).T
+        self.table8.index = ['a', 'b', 'c',
+                             'd', 'e', 'f']
+        self.cats8 = pd.Series([0, 0, 1, 0, 1, 1],
+                               index=['a', 'b', 'd',
+                                      'c', 'e', 'f'])
+
+        # Real valued data with 3 groupings
+        D, L = 40, 120
+        np.random.seed(0)
+        self.table9 = np.vstack((np.concatenate((normal(10, 1, D),
+                                                 normal(200, 1, D),
+                                                 normal(400, 1, D))),
+                                 np.concatenate((normal(200000, 1, D),
+                                                 normal(10, 1, D),
+                                                 normal(2000, 1, D))),
+                                 normal(10, 10, L),
+                                 normal(10, 10, L),
+                                 np.concatenate((normal(2000, 1, D),
+                                                 normal(100000, 1, D),
+                                                 normal(2000, 1, D))),
+                                 normal(10000, 1000, L),
+                                 normal(10, 10, L),
+                                 normal(10, 10, L),
+                                 normal(10, 10, L),
+                                 normal(10000, 1000, L),
+                                 normal(10, 10, L),
+                                 normal(10, 10, L),
+                                 normal(10, 10, L),
+                                 np.concatenate((normal(2000, 1, D),
+                                                 normal(100000, 1, D),
+                                                 normal(2000, 1, D))),
+                                 normal(10000, 1000, L),
+                                 normal(10, 10, L),
+                                 normal(10, 10, L),
+                                 normal(10, 10, L)))
+        self.table9 = np.absolute(self.table9)+1
+        self.table9 = pd.DataFrame(self.table9.astype(np.int).T)
+        self.cats9 = pd.Series([0]*D + [1]*D + [2]*D)
+
+        # Real valued data with 2 groupings
+        D, L = 40, 80
+        np.random.seed(0)
+        self.table10 = np.vstack((np.concatenate((normal(10, 1, D),
+                                                  normal(200, 1, D))),
+                                  np.concatenate((normal(10, 1, D),
+                                                  normal(200, 1, D))),
+                                  np.concatenate((normal(20, 10, D),
+                                                  normal(100, 10, D))),
+                                  normal(10, 1, L),
+                                  np.concatenate((normal(200, 100, D),
+                                                  normal(100000, 100, D))),
+                                  np.concatenate((normal(200000, 100, D),
+                                                  normal(300, 100, D))),
+                                  np.concatenate((normal(200000, 100, D),
+                                                  normal(300, 100, D))),
+                                  np.concatenate((normal(20, 20, D),
+                                                  normal(40, 10, D))),
+                                  np.concatenate((normal(20, 20, D),
+                                                  normal(40, 10, D))),
+                                  np.concatenate((normal(20, 20, D),
+                                                  normal(40, 10, D))),
+                                  normal(100, 10, L),
+                                  normal(100, 10, L),
+                                  normal(1000, 10, L),
+                                  normal(1000, 10, L),
+                                  normal(10, 10, L),
+                                  normal(10, 10, L),
+                                  normal(10, 10, L),
+                                  normal(10, 10, L)))
+        self.table10 = np.absolute(self.table10) + 1
+        self.table10 = pd.DataFrame(self.table10.astype(np.int).T)
+        self.cats10 = pd.Series([0]*D + [1]*D)
+
+        # zero count
+        self.bad1 = pd.DataFrame(np.array([
+            [10, 10, 10, 20, 20, 0],
+            [11, 11, 11, 21, 21, 21],
+            [10, 10, 10, 10, 10, 10],
+            [10, 10, 10, 10, 10, 10],
+            [10, 10, 10, 10, 10, 10],
+            [10, 10, 10, 10, 10, 10],
+            [10, 10, 10, 10, 10, 10]]).T)
+        # negative count
+        self.bad2 = pd.DataFrame(np.array([
+            [10, 10, 10, 20, 20, 1],
+            [11, 11, 11, 21, 21, 21],
+            [10, 10, 10, 10, 10, 10],
+            [10, 10, 10, 10, 10, 10],
+            [10, 10, 10, 10, 10, 10],
+            [10, 10, 10, 10, 10, -1],
+            [10, 10, 10, 10, 10, 10]]).T)
+
+        # missing count
+        self.bad3 = pd.DataFrame(np.array([
+            [10, 10, 10, 20, 20, 1],
+            [11, 11, 11, 21, 21, 21],
+            [10, 10, 10, 10, 10, 10],
+            [10, 10, 10, 10, 10, 10],
+            [10, 10, 10, 10, 10, 10],
+            [10, 10, 10, 10, 10, np.nan],
+            [10, 10, 10, 10, 10, 10]]).T)
+        self.badcats1 = pd.Series([0, 0, 0, 1, np.nan, 1])
+        self.badcats2 = pd.Series([0, 0, 0, 0, 0, 0])
+        self.badcats3 = pd.Series([0, 0, 1, 1])
+        self.badcats4 = pd.Series(range(len(self.table1)))
+        self.badcats5 = pd.Series([1]*len(self.table1))
+
+    def test_ancom_fail_missing(self):
+        with self.assertRaises(ValueError):
+            balance_test(self.bad3, self.cats1, self.tree1)
+
+        with self.assertRaises(ValueError):
+            balance_test(self.table1, self.badcats1, self.tree1)
+
+    def test_ancom_fail_groups(self):
+        with self.assertRaises(ValueError):
+            balance_test(self.table1, self.badcats2, self.tree1)
+
+    def test_ancom_fail_size_mismatch(self):
+        with self.assertRaises(ValueError):
+            balance_test(self.table1, self.badcats3, self.tree1)
+
+    def test_ancom_fail_group_unique(self):
+        with self.assertRaises(ValueError):
+            balance_test(self.table1, self.badcats4, self.tree1)
+
+    def test_ancom_fail_1_group(self):
+        with self.assertRaises(ValueError):
+            balance_test(self.table1, self.badcats5, self.tree1)
+
+if __name__ == '__main__':
+    unittest.main()
